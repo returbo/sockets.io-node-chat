8d28018fdb362ae01a14eba2bd5a9744d83967e5
returbo <returbo@mail.ru>
2020-06-19 16:42:14 +1000

    start dev server

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import express from "express";
-import server from 'http';
-import io from 'socket.io';
+import http from 'http';
 
 const app = express();
+// let server = require('http').createServer(app);
+const server = http.createServer(app);
+let io = require('socket.io').listen(server);
 
-server = server.createServer(app);
-io = io.listen(server);
 
 server.listen(3000);
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
+});
+
+const users = [];
+const connections = [];
+
+io.sockets.on('connection', function(socket) {
+    console.log("Все норм, подключились")
+    connections.push(socket);
+
+    socket.on('disconnect', function(data) {
+        connections.splice(connections.indexOf(socket), 1);
+    });
+
 });
