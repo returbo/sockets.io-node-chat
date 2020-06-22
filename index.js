79ef360ff4e34458254ea62b4b093f99ec725dc2
returbo <returbo@mail.ru>
2020-06-22 14:25:55 +1000

    stable version

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-import express from "express";
-import http from 'http';
-
-const app = express();
-// let server = require('http').createServer(app);
-const server = http.createServer(app);
-let io = require('socket.io').listen(server);
-
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 server.listen(3000);
 
@@ -13,15 +9,24 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
 });
 
-const users = [];
-const connections = [];
+users = [];
+connections = [];
 
 io.sockets.on('connection', function(socket) {
-    console.log("Все норм, подключились")
+
+    console.log("Все норм, подключились");
     connections.push(socket);
 
-    socket.on('disconnect', function(data) {
+    socket.on('disconnect', function() {
         connections.splice(connections.indexOf(socket), 1);
+        console.log('Отключились');
     });
 
+    socket.on('send mess', function(data) {
+        io.sockets.emit('add mess', { 
+            mess: data.mess, 
+            name: data.name,
+            className: data.className
+        });
+    });
 });
